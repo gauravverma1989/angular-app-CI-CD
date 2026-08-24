99fbb98a77b3cbdcf9a04ef1e1368341c2e11f7f
Gaurav Verma <gaurav.verma@ngfvtx.onmicrosoft.com>
2026-08-24 16:56:31 +0530

    home page text updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = ;
+  title = 'ADDED CI CD with unit test pipelines successfully';
 }
