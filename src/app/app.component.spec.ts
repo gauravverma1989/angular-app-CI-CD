ab3875bf7c80f87f31318788bda135e20768504f
Gaurav Verma <gaurav.verma@ngfvtx.onmicrosoft.com>
2026-08-24 17:10:23 +0530

    test worked successfully

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'test-angular-app' title`, () => {
+  it(`should have the 'CI CD INTEGRATION COMPLETED SUCCESSFULLY' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-angular-app');
+    expect(app.title).toEqual('CI CD INTEGRATION COMPLETED SUCCESSFULLY');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('CD CD INTEGRATION COMPLETED SUCCESSFULLY');
+    expect(compiled.querySelector('h1')?.textContent).toContain('CI CD INTEGRATION COMPLETED SUCCESSFULLY');
   });
 });
